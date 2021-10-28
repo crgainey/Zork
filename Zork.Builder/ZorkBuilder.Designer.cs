36bac34f5c7b4dda81b91e6d3bf8048304381c10
Mikaeyla <70242999+pechy28@users.noreply.github.com>
2021-10-27 22:24:35 -0400

    Zork Windows Forms pt. 1

---
+++ Zork.Builder/ZorkBuilder.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace Zork.Builder
+{
+    partial class ZorkBuilder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.MenuStrip mainMenuStrip;
+            System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+            System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+            System.Windows.Forms.ToolStripMenuItem newFileToolStripMenuItem;
+            System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+            System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+            System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+            System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+            System.Windows.Forms.ToolStripSeparator toolStripMenuItem4;
+            System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.roomsList = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.roomDescriptionText = new System.Windows.Forms.RichTextBox();
+            this.worldTabs = new System.Windows.Forms.TabControl();
+            this.neighborsList = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.welcomeMessageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.startingLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            newFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            toolStripMenuItem4 = new System.Windows.Forms.ToolStripSeparator();
+            exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.worldTabs.SuspendLayout();
+            mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.deleteButton);
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.roomsList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 455);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rooms";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(232, 407);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(151, 407);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // roomsList
+            // 
+            this.roomsList.FormattingEnabled = true;
+            this.roomsList.Location = new System.Drawing.Point(7, 20);
+            this.roomsList.Name = "roomsList";
+            this.roomsList.Size = new System.Drawing.Size(305, 381);
+            this.roomsList.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.roomDescriptionText);
+            this.groupBox2.Controls.Add(this.worldTabs);
+            this.groupBox2.Location = new System.Drawing.Point(348, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(329, 455);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Room Attributes";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Description";
+            // 
+            // roomDescriptionText
+            // 
+            this.roomDescriptionText.Location = new System.Drawing.Point(6, 315);
+            this.roomDescriptionText.Name = "roomDescriptionText";
+            this.roomDescriptionText.Size = new System.Drawing.Size(313, 134);
+            this.roomDescriptionText.TabIndex = 1;
+            this.roomDescriptionText.Text = "";
+            // 
+            // worldTabs
+            // 
+            this.worldTabs.Controls.Add(this.neighborsList);
+            this.worldTabs.Controls.Add(this.tabPage2);
+            this.worldTabs.Location = new System.Drawing.Point(6, 20);
+            this.worldTabs.Name = "worldTabs";
+            this.worldTabs.SelectedIndex = 0;
+            this.worldTabs.Size = new System.Drawing.Size(317, 273);
+            this.worldTabs.TabIndex = 0;
+            // 
+            // neighborsList
+            // 
+            this.neighborsList.Location = new System.Drawing.Point(4, 22);
+            this.neighborsList.Name = "neighborsList";
+            this.neighborsList.Padding = new System.Windows.Forms.Padding(3);
+            this.neighborsList.Size = new System.Drawing.Size(309, 247);
+            this.neighborsList.TabIndex = 0;
+            this.neighborsList.Text = "Neighbors";
+            this.neighborsList.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(309, 247);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Items";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // mainMenuStrip
+            // 
+            mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            fileToolStripMenuItem,
+            editToolStripMenuItem,
+            this.runToolStripMenuItem});
+            mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            mainMenuStrip.Name = "mainMenuStrip";
+            mainMenuStrip.Size = new System.Drawing.Size(689, 24);
+            mainMenuStrip.TabIndex = 1;
+            mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            newFileToolStripMenuItem,
+            openFileToolStripMenuItem,
+            toolStripMenuItem3,
+            saveFileToolStripMenuItem,
+            saveAsToolStripMenuItem,
+            toolStripMenuItem4,
+            exitToolStripMenuItem});
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.welcomeMessageToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.startingLocationToolStripMenuItem});
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            editToolStripMenuItem.Text = "&Edit";
+            // 
+            // welcomeMessageToolStripMenuItem
+            // 
+            this.welcomeMessageToolStripMenuItem.Name = "welcomeMessageToolStripMenuItem";
+            this.welcomeMessageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.welcomeMessageToolStripMenuItem.Text = "Welcome Message";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // startingLocationToolStripMenuItem
+            // 
+            this.startingLocationToolStripMenuItem.Name = "startingLocationToolStripMenuItem";
+            this.startingLocationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.startingLocationToolStripMenuItem.Text = "Starting Location";
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.runToolStripMenuItem.Text = "&Run";
+            // 
+            // newFileToolStripMenuItem
+            // 
+            newFileToolStripMenuItem.Name = "newFileToolStripMenuItem";
+            newFileToolStripMenuItem.ShortcutKeyDisplayString = "";
+            newFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            newFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            newFileToolStripMenuItem.Text = "&New File";
+            newFileToolStripMenuItem.Click += new System.EventHandler(this.newFileToolStripMenuItem_Click);
+            // 
+            // openFileToolStripMenuItem
+            // 
+            openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            openFileToolStripMenuItem.ShortcutKeyDisplayString = "";
+            openFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            openFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            openFileToolStripMenuItem.Text = "&Open File";
+            openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            saveFileToolStripMenuItem.ShortcutKeyDisplayString = "";
+            saveFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            saveFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            saveFileToolStripMenuItem.Text = "&Save File";
+            saveFileToolStripMenuItem.Click += new System.EventHandler(this.saveFileToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            saveAsToolStripMenuItem.Text = "Save As";
+            saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            toolStripMenuItem4.Name = "toolStripMenuItem4";
+            toolStripMenuItem4.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.ShortcutKeyDisplayString = "";
+            exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            exitToolStripMenuItem.Text = "E&xit";
+            exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "JSON Files|*.json";
+            // 
+            // ZorkBuilder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 494);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(mainMenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = mainMenuStrip;
+            this.MaximizeBox = false;
+            this.Name = "ZorkBuilder";
+            this.ShowInTaskbar = false;
+            this.Text = "Zork Builder";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.worldTabs.ResumeLayout(false);
+            mainMenuStrip.ResumeLayout(false);
+            mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ListBox roomsList;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolStripMenuItem welcomeMessageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem startingLocationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox roomDescriptionText;
+        private System.Windows.Forms.TabControl worldTabs;
+        private System.Windows.Forms.TabPage neighborsList;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Zork.Builder/ZorkBuilder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zork;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Zork.Builder
+{
+    public partial class ZorkBuilder : Form
+    {
+        public ZorkBuilder()
+        {
+            InitializeComponent();
+        }
+
+
+        
+
+        private void newFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string jsonString = File.ReadAllText(openFileDialog.FileName);
+                JsonConvert.DeserializeObject<World>(jsonString);
+            }
+        }
+
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
