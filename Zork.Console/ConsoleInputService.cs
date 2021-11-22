0c933b4a3bd318652d65197d085ddade34e87bdf
Cynthia Gainey <54631702+crgainey@users.noreply.github.com>
2021-11-21 23:03:46 -0500

    Zork Unity 2.1

---
+++ Zork.Console/ConsoleInputService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zork
+{
+    class ConsoleInputService : IInputService
+    {
+        public event EventHandler<string> InputRecieved;
+
+        public void ProcessInput()
+        {
+            string inputString = Console.ReadLine().Trim().ToUpper();
+            InputRecieved?.Invoke(this, inputString);
+        }
+    }
+}
